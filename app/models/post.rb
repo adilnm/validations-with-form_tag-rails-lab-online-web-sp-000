c87689057de5d141871bcd4c3a9fe08fb0fe09ba
ADIL NAIMI <adilnm23@hotmail.com>
2019-09-21 07:20:11 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 10 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
 end
