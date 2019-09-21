9f18efe6604d535e401a3eceebb53db07e7e7f7d
ADIL NAIMI <adilnm23@hotmail.com>
2019-09-21 07:24:41 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,7 +11,7 @@ class AuthorsController < ApplicationController
     
     @author = Author.create(author_params)
     if @author.valid?
-      @post.save
+      @author.save
       redirect_to author_path(@author)
     else
       render:new
